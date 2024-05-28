25211588baa2058d817964bbeee6a56c146cb669
ferreiraz <business.flavioferreira@gmail.com>
2024-05-28 08:34:11 -0300

    feat: apply tddd

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,57 +1,61 @@
 import axios from "axios";
 
 axios.defaults.validateStatus = function () {
-	return true;
+	return true
 }
 
-test("Não deve fazer um pedido com cpf inválido", async function () {
+test("Não deve fazer pedido com cpf invalido", async function () {
 	const input = {
-		cpf: "987.654.321-01"
+		cpf: "144.796.170-10"
 	};
-	const response = await axios.post("http://localhost:3000/checkout", input)
-	expect(response.status).toBe(422);
-	const output = response.data;
-	expect(output.message).toBe("Invalid cpf");
-});
 
-test("Deve fazer um pedido com 3 produtos", async function () {
+	const response = await axios.post("http://localhost:3000/checkout", input)
+	expect(response.status).toBe(422)
+	const output = response.data
+	expect(output.message).toBe("invalid cpf")
+})
+
+test("Deve fazer pedido com 3 produtos", async function () {
 	const input = {
 		cpf: "987.654.321-00",
 		items: [
-			{ idProduct: 1, quantity: 1 },
-			{ idProduct: 2, quantity: 1 },
-			{ idProduct: 3, quantity: 3 }
+			{idProduct: 1, quantity: 1},
+			{idProduct: 2, quantity: 1},
+			{idProduct: 3, quantity: 1},
 		]
 	};
-	const response = await axios.post("http://localhost:3000/checkout", input)
-	const output = response.data;
-	expect(output.total).toBe(6090);
-});
 
-test("Não deve fazer pedido com produto que não existe", async function () {
+	const response = await axios.post("http://localhost:3000/checkout", input)
+	const output = response.data
+	expect(output.total).toBe(6090)
+})
+
+test("Não deve fazer pedido com produto inesxistente", async function () {
 	const input = {
 		cpf: "987.654.321-00",
 		items: [
-			{ idProduct: 4, quantity: 1 }
+			{idProduct: 4, quantity: 1},
 		]
 	};
-	const response = await axios.post("http://localhost:3000/checkout", input)
-	expect(response.status).toBe(422);
-	const output = response.data;
-	expect(output.message).toBe("Product not found");
-});
 
-test("Deve fazer um pedido com 3 produtos com cupom de desconto", async function () {
+	const response = await axios.post("http://localhost:3000/checkout", input)
+	expect(response.status).toBe(422)
+	const output = response.data
+	expect(output.message).toBe("product not found")
+})
+
+test("Deve fazer pedido com 3 produtos com cupom de desconto", async function () {
 	const input = {
 		cpf: "987.654.321-00",
 		items: [
-			{ idProduct: 1, quantity: 1 },
-			{ idProduct: 2, quantity: 1 },
-			{ idProduct: 3, quantity: 3 }
+			{idProduct: 1, quantity: 1},
+			{idProduct: 2, quantity: 1},
+			{idProduct: 3, quantity: 1},
 		],
 		coupon: "VALE20"
 	};
+
 	const response = await axios.post("http://localhost:3000/checkout", input)
-	const output = response.data;
-	expect(output.total).toBe(4872);
-});
+	const output = response.data
+	expect(output.total).toBe(4872)
+})
